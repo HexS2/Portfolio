f5101760b80741d6bb1e2f14967f0f770c8f8e72
Guillaume <inoxsdev@gmail.com>
2021-10-30 01:47:00 +0200

    working skeleton
    base project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import React from 'react'
+import { ChakraProvider, ColorModeProvider,UseColorMode } from "@chakra-ui/react"
+import  customTheme from '../styles/theme'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider options={{
+        initialColorMode:"dark",
+        useSystemColorMode:true,
+      }}>
+      </ColorModeProvider>
+
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
