35d1e90a05dc1a796d27332d4ca3c2f1a2b9f346
Guillaume <inoxsdev@gmail.com>
2021-11-01 18:14:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,78 +25,68 @@ export default function Index() {
       <Head>
         <title>Home - h3x</title>
       </Head>
-      <Stack
-        as="main"
-        spacing={8}
+
+      <Flex
+        flexDirection="column"
         justifyContent="center"
-        alignItems="flex-start"
-        m="0 auto 4rem auto"
-        minW="container.sm"
-        px={2}
+        alignItems="center"
+        marginTop="60px"
       >
-        <Flex
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          marginTop="60px"
-          minW="container.sm"
-        >
-          <WrapItem>
-            <Avatar
-              name="Segun Adebayo"
-              size="2xl"
-              src="https://avatars.githubusercontent.com/u/18382518?s=400&u=8f2cc93898a29da6c3c4cedfb36be2e90f14d608&v=4"
-            />
-          </WrapItem>
-          <Heading mb={2}>Hi, Im h3x</Heading>
-          <Text color={colorSecondary[colorMode]}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante
-            nunc, finibus sit amet purus quis, congue vulputate ipsum. Phasellus
-            lobortis bibendum orci, quis imperdiet lectus imperdiet porttitor.
-          </Text>
-          <HStack padding="70px">
-            <Button colorScheme="white" variant="outline">
-              <Link href="https://github.com/HexS2" isExternal>
-                <Icon
-                  as={GoOctoface}
-                  w={8}
-                  h={8}
-                  m={1}
-                  color={colorSecondary[colorMode]}
-                />{" "}
-                GitHub
-              </Link>
-            </Button>
-            <Button colorScheme="white" variant="outline">
-              <Link href="https://twitter.com/CTRLfr" isExternal>
-                <Icon
-                  as={FaTwitter}
-                  w={8}
-                  h={8}
-                  m={1}
-                  color={colorSecondary[colorMode]}
-                />{" "}
-                Twitter
-              </Link>
-            </Button>
-            <Button colorScheme="white" variant="outline">
-              <Link
-                href="https://www.linkedin.com/in/guillaume-lecareux-1456671a6/"
-                isExternal
-              >
-                <Icon
-                  as={FaLinkedin}
-                  w={8}
-                  h={8}
-                  m={1}
-                  color={colorSecondary[colorMode]}
-                />{" "}
-                Linkedin
-              </Link>
-            </Button>
-          </HStack>
-        </Flex>
-      </Stack>
+        <WrapItem>
+          <Avatar
+            name="Segun Adebayo"
+            size="2xl"
+            src="https://avatars.githubusercontent.com/u/18382518?s=400&u=8f2cc93898a29da6c3c4cedfb36be2e90f14d608&v=4"
+          />
+        </WrapItem>
+        <Heading mb={2}>Hi, Im h3x</Heading>
+        <Text color={colorSecondary[colorMode]}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante nunc,
+          finibus sit amet purus quis, congue vulputate ipsum. Phasellus
+          lobortis bibendum orci, quis imperdiet lectus imperdiet porttitor.
+        </Text>
+        <HStack padding="70px">
+          <Button colorScheme="white" variant="outline">
+            <Link href="https://github.com/HexS2" isExternal>
+              <Icon
+                as={GoOctoface}
+                w={8}
+                h={8}
+                m={1}
+                color={colorSecondary[colorMode]}
+              />{" "}
+              GitHub
+            </Link>
+          </Button>
+          <Button colorScheme="white" variant="outline">
+            <Link href="https://twitter.com/CTRLfr" isExternal>
+              <Icon
+                as={FaTwitter}
+                w={8}
+                h={8}
+                m={1}
+                color={colorSecondary[colorMode]}
+              />{" "}
+              Twitter
+            </Link>
+          </Button>
+          <Button colorScheme="white" variant="outline">
+            <Link
+              href="https://www.linkedin.com/in/guillaume-lecareux-1456671a6/"
+              isExternal
+            >
+              <Icon
+                as={FaLinkedin}
+                w={8}
+                h={8}
+                m={1}
+                color={colorSecondary[colorMode]}
+              />{" "}
+              Linkedin
+            </Link>
+          </Button>
+        </HStack>
+      </Flex>
     </Container>
   );
 }
