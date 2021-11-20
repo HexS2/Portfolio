0f02061700f9f68e4a31504fbee447612459f632
Guillaume <inoxsdev@gmail.com>
2021-11-20 19:38:39 +0100

    updated work

--- pages/work.js
+++ pages/work.js
@@ -1,5 +1,5 @@
 import Container from "../components/Container";
-import { Box, Alert, AlertTitle, AlertDescription } from "@chakra-ui/react";
+import { Box,Wrap,Flex,WrapItem,Avatar,Heading} from "@chakra-ui/react";
 import { ImSmile } from "react-icons/im";
 import { CloseButton } from "@chakra-ui/close-button";
 import React from "react";
@@ -7,20 +7,29 @@ export default function Work() {
   const [display, setDisplay] = React.useState("true");
   return (
     <Container>
-      <Alert status="warning" display={display}>
-        <Box flex="1">
-          <AlertTitle>Hey !</AlertTitle>
-          <AlertDescription display="block">
-            This page is under construction, come back soon ! <ImSmile />
-          </AlertDescription>
-        </Box>
-        <CloseButton
-          onClick={() => setDisplay("none")}
-          position="absolute"
-          right="8px"
-          top="8px"
-        />
-      </Alert>
+              <Box   flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        marginTop="60px">       
+        <Heading> Mes projets</Heading>
+</Box>
+ <Flex
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        marginTop="60px"
+        marginBottom="100px"
+      >
+        <Wrap>
+  <WrapItem>
+    <Avatar size="xl" backgroundColor="transparent"  name="Bloodshed" src="https://pbs.twimg.com/profile_images/1206171910687600640/s_W-uL2b_400x400.jpg" />
+  </WrapItem>
+  <WrapItem>
+    <Avatar size="xl" backgroundColor="transparent"  name="Tolarys" src="https://pbs.twimg.com/profile_images/1077186393368080386/_8EA6n81_400x400.jpg" />
+  </WrapItem>
+  </Wrap>
+      </Flex>
     </Container>
   );
 }
